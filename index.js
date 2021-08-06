acebfac86a52e5d76e38a5edfdf0e4f767d21663
Vishal Hasnani Mac <hasnanivishal@gmail.com>
2021-08-06 16:26:09 +0530

    logs added.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.get("/topshow", async (req, res, next) => {
     try {
-
+        console.log('started....');
     const chromeOptions = {
             headless: true,
             defaultViewport: null,
@@ -23,27 +23,42 @@ app.get("/topshow", async (req, res, next) => {
 
     const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
-
+    console.log('calling the website....');
     await page.goto('https://www.binged.com/streaming-premiere-dates/mimi-movie-streaming-online-watch-2/');
+    console.log('called the website....');
     const textsArray = await page.evaluate(() => document.querySelector('body').innerHTML);
+    console.log('textsArray is ready....');
     let abc = getData(textsArray);
+    console.log('abc is ready....', abc.length);
     let finalOutput = [];
 
-    for (let index = 0; index < 20; index++) {
+    for (let index = 0; index < 15; index++) {
         finalOutput.push({
             name: abc[index].name,
             image: getCorrectImageUrl(abc[index].image)
-        })
+        });
+        console.log('finalOutput for index is done', index);
     }
 
     console.log(finalOutput);
-
     await browser.close();
 
-    res.json(finalOutput);
+    return res.send(
+        {
+           status: 200,
+           result: finalOutput
+        }
+     )
 
     } catch (error) {
-        res.json('error occured.....', error);
+        console.error('Failed to fetch data:', error);
+
+         return res.send(
+            {
+               status: 500,
+               result: error
+            }
+         )
     }
 });
 
