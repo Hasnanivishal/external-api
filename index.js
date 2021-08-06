01598e9850dbc9f51d8e9ee0ea36d7888fc47466
Vishal Hasnani Mac <hasnanivishal@gmail.com>
2021-08-06 15:15:06 +0530

    trying puppeteer

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var express = require("express");
 var app = express();
+const puppeteer = require('puppeteer');
+
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
+
+app.get("/topshow", async (req, res, next) => {
+    try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://scrapethissite.com/pages/forms/');
+    const textsArray = await page.evaluate(
+        () => [...document.querySelectorAll(
+            '#hockey > div > table > tbody > tr > td.name')]
+            .map(elem => elem.innerText)
+    );
+    const WinArray = await page.evaluate(
+        () => [...document.querySelectorAll(
+            '#hockey > div > table > tbody > tr > td.wins')]
+            .map(elem => elem.innerText)
+    );
+    var result = {};
+    textsArray.forEach((textsArray, i) =>
+        result[textsArray] = WinArray[i]);
+    console.log(result);
+
+    await browser.close();
+
+    res.json(result);
+    } catch (error) {
+        res.json('error occured..');
+    }
+});
+   
